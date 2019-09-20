3b38f19d59d8e92d8e1bcf9ba0213b58c5e227aa
sreepriyaexperion <sreepriya.ck@experionglobal.com>
2019-09-20 10:26:44 +0530

    my-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
